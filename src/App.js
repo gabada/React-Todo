ddc9d6029e10d022de0d06c73f07e004653e2f4f
Gill Abada <gill@callifi.com>
2019-02-13 17:11:26 -0500

    refactored methods and todo form add inputText state.

--- src/App.js
+++ src/App.js
@@ -5,24 +5,35 @@ class App extends React.Component {
   constructor() {
     super();
     this.state={
-      task: []
+      tasks: [],
+      inputText: ''
     };
   }
 
   addTask = e => {
     e.preventDefault();
-    // if STATEMENT
     this.setState({
+      tasks: [
+        ...this.state.tasks,
+        { task: this.state.inputText, id: Date.now(), completed: false}
+      ],
+      inputText: ''
+    });
 
-    })
   }
+
+  handleChanges = e => {
+    this.setState({
+      inputText: e.target.value
+    });
+  };
 
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
         <TodoForm 
-          task={this.task}
+          inputText={this.state.inputText}
           handleChanges={this.handleChanges}
           addTask={this.addTask}
         />
@@ -32,22 +43,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-
-// [
-//   {
-//     task: 'Organize Garage',
-//     id: 1528817077286,
-//     completed: false
-//   },
-//   {
-//     task: 'Bake Cookies',
-//     id: 1528817084358,
-//     completed: false
-//   }
-// ];
-
-
-// you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
