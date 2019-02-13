8107ad80a740bfd768af1e0564c6284df1cc22d6
Gill Abada <gill@callifi.com>
2019-02-13 17:55:06 -0500

    complete day 1 MVP.

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Todo from './Todo';
 
 const TodoList = props => {
     return (
         <ul>
             {props.tasks.map(task => (
                 <Todo
-                    task={task}
+                    task={task.task}
                     key={task.id}
                 />
             ))}
